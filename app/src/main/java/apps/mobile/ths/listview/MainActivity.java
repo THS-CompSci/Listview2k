04a0f89b8d47ff8479ca9948773d918fe0aba98f
thscs200 <chrishudson@katyisd.org>
2015-11-11 12:18:51 -0600

    committing test

--- app/src/main/java/apps/mobile/ths/listview/MainActivity.java
+++ app/src/main/java/apps/mobile/ths/listview/MainActivity.java
@@ -2,6 +2,7 @@ package apps.mobile.ths.listview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,24 +18,28 @@ import java.util.Arrays;
 public class MainActivity extends Activity implements OnItemClickListener {
     private ListView listView1;
     private ArrayAdapter<String> listAdapter1;
+    private String [] someColors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView1 = (ListView) findViewById(R.id.listView);
-        String[] someColors = new String[]{"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet", "Black", "White"};
+        someColors = new String[]{"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet", "Black", "White"};
         ArrayList<String> colorArrayList = new ArrayList<String>();
         colorArrayList.addAll(Arrays.asList(someColors));
-        listAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, colorArrayList);
+        listAdapter1 = new ArrayAdapter<String>(this, R.layout.simple_list_item_1, colorArrayList);
         listView1.setAdapter(listAdapter1);
         listView1.setOnItemClickListener(this);
     }
 
-    //another comment
+
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String itemValue = (String) listView1.getItemAtPosition(position);
-        Toast.makeText(getApplicationContext(), itemValue, Toast.LENGTH_LONG).show();
+        String itemValue = (String) listView1.getItemAtPosition((int)id);
+        Log.i("MainActivity", "position: " + position);
+        Log.i("MainActivity", "id: " + id);
+        Toast.makeText(getApplicationContext(),someColors[position] , Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(), itemValue, Toast.LENGTH_LONG).show();
     }
 }
